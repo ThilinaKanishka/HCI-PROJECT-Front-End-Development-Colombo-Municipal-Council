7b4cc29021a7f4a510970cb90017b5c0e367f855
ThilinaKanishka <thilina20010313@gmail.com>
2025-08-12 23:52:50 +0530

    instal react-router-dom

---
+++ Front_End/src/components/Engineering_Services.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function Engineering_Services() {
+  return (
+    <div>
+      <h1>Engineering Services</h1>
+    </div>
+  );
+}
+
+export default Engineering_Services;
