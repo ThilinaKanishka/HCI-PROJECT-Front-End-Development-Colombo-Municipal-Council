9c68c2231ea832a88e52c7fed1d41f36b7a0887b
ThilinaKanishka <thilina20010313@gmail.com>
2025-09-22 15:25:26 +0530

    update page pathe

--- Front_End/src/App.jsx
+++ Front_End/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Engineering_Services from "./components/Engineering_Services";
 import Home from "./components/Home";
+import Social_Services from "./components/Social_Services";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
           path="/Engineering_Services"
           element={<Engineering_Services />}
         />
+        <Route path="/Social_Services" element={<Social_Services />} />
       </Routes>
     </Router>
   );
