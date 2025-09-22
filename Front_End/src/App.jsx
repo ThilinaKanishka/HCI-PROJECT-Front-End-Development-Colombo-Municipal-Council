cf900acc3ea26675deb66a66887c96f789382b3b
ThilinaKanishka <thilina20010313@gmail.com>
2025-09-22 15:23:21 +0530

    Update App.jsx

--- Front_End/src/App.jsx
+++ Front_End/src/App.jsx
@@ -1,11 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Engineering_Services from "./components/Engineering_Services";
+import Home from "./components/Home";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Engineering_Services />} />
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/Engineering_Services"
+          element={<Engineering_Services />}
+        />
       </Routes>
     </Router>
   );
